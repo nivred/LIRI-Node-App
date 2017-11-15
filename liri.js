ad1f1b5732d822ddd5b445e4c95c3eeeccf48e60
nivred <emaildervin@gmail.com>
2017-11-14 18:43:25 -0600

    edits made to spotify and OMDB functions

--- liri.js
+++ liri.js
@@ -1,9 +1,12 @@
-var keys = require("./keys.js")
+var keys = require("./keys.js");
+var fs = require("fs");
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var request = require('request');
 
 var nodeArg = process.argv[2];
+var queryString = ""
+
 
 var client = new Twitter({
     consumer_key: keys.twitterKeys.consumer_key,
@@ -33,14 +36,12 @@ function myTweets() {
 }
 
 function spotifyThisSong() {
-    var queryString = ""
     if (process.argv.length - 3 > 0) {
         for (var i=3; i < process.argv.length; i++) {
             queryString += process.argv[i] + " ";
         }
     } else {
-        queryString = "The Sign"
-        console.log(queryString);
+        queryString = "The Sign Ace of Base"
     }
 
     spotify.search({ type: 'track', query: queryString, limit: 1 }, function(err, data) {
@@ -49,55 +50,52 @@ function spotifyThisSong() {
         }
         for (var i=0; i < data.tracks.items.length; i++) {
             var item = data.tracks.items[i]
-            console.log("\nArtist(s): " + item.name);
-            console.log("Song Name: " + item.artists[0].name);
+            console.log("\nArtist(s): " + item.artists[0].name);
+            console.log("Song Name: " + item.name);
             console.log("Preview Song: " + item.artists[0].external_urls.spotify);
-            console.log("Album Name: " +item.album.name);            
+            console.log("Album Name: " + item.album.name + "\n");            
         }
-      console.log(data.tracks.items[0]); 
+    //   console.log(data.tracks.items[0]); 
     });  
 }
 
 function movieThis() {
-    var queryString = ""
     if (process.argv.length - 3 > 0) {
         for (var i=3; i < process.argv.length; i++) {
             queryString += process.argv[i] + " ";
         }
     } else {
-        queryString = "Mr. Nobody"
-        console.log(queryString);
+        console.log("\nIf you haven't watched " + "'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+        console.log("It's on Netflix!");
+        queryString = "Mr. Nobody";
     }
 
     request('https://www.omdbapi.com/?t=' + queryString + '&y=&plot=short&apikey=40e9cece', function (error, response, body) {
-        // console.log('body:', body);
-        var bodyArr = JSON.parse(body);
-        // console.log(bodyArr);
-        for (var i=0; i < bodyArr.length; i++) {
-            var info = bodyArr[i]
-            console.log(info);
-
-            console.log("\nMovie Title: " + bodyArr.Title);
-            console.log("Released: " + bodyArr.Released);
-            console.log("IMDB Rating: " + info.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + info.imdbRating);
-            console.log("Country Produced: " + info.Country);
-            console.log("Language: " + info.language);
-            console.log("Movie Plot: " + info.Plot);
-            console.log("Movie Cast: " + info.Actors);
+        
+        if (!error && response.statusCode === 200) {
+            var movie = JSON.parse(body);
+            console.log("\nMovie Title: " + movie.Title);
+            console.log("Released: " + movie.Released);
+            console.log("IMDB Rating: " + movie.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + movie.Ratings[1].Value);
+            console.log("Country Produced: " + movie.Country);
+            console.log("Language: " + movie.Language);
+            console.log("Movie Plot: " + movie.Plot);
+            console.log("Movie Cast: " + movie.Actors + "\n");
         }
-    });
-    
+    }); 
 }
 
-// function doWhatItSays() {
-//     var fs = require(“fs”);
-//     fs.readFile()
-
-//     if (err) {
-//         return console.log(err);
-//     }    
-// }
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(", ");
+        queryString = dataArr[0];
+    }); 
+    console.log(queryString);
+}
 
 switch(nodeArg) {
     case "my-tweets":
@@ -114,5 +112,4 @@ switch(nodeArg) {
         break;
     default:
         console.log("Please select valid command");
-    
 }
